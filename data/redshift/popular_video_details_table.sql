b89f9a1a4a6f640c029db902696b998541094020
Bayanna Anduri <94741815+Bayanna512@users.noreply.github.com>
2024-08-09 20:44:59 +0800

    Update popular_video_details_table.sql
    ddl change

--- data/redshift/popular_video_details_table.sql
+++ data/redshift/popular_video_details_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE popular_video_details (
     ChannelID VARCHAR(50),
     VideoID VARCHAR(50),
-    Title VARCHAR(500),
+    Title VARCHAR(1000),
     ViewCount BIGINT,
     PublishDate TIMESTAMP
 );
